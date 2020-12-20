340af396f9a4004e37b1982cb30faa3c96425656
mxleach <max.leach208@gmail.com>
2020-12-19 19:01:17 -0500

    Revised

--- js-exercises2.js
+++ js-exercises2.js
@@ -1,12 +1,13 @@
 // ---------------------
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 function max(a,b) {
-    if a > b {
-    return a;
+    if (a > b) {
+        return a;
     } else {
         return b;
-        }
     }
+}
+    
 
 // ---------------------
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
@@ -44,20 +45,15 @@ function translate()
 // Define a function sum() and a function multiply() that sums and multiplies (respectively)
 //all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10,
 // and multiply([1,2,3,4]) should return 24.
-function sum([a,b,c,d]) {
-    let total = 0;
-    for (let i = 0; i < [a,b,c,d].length; i++) {
-        total += [a,b,c,d][i];
-    }
-    return total;
+function sum(numbers) {
+    return numbers.reduce((a,b) => {return a+b}, 0) 
+
+
 }
 
-function multiply([a,b,c,d]) {
-    let total = 1;
-    for (let i=0; i < [a,b,c,d].length; i++) {
-        total =  total*[a,b,c,d] [i]
-    }
-    return total;
+
+function multiply(numbers) {
+    return numbers.reduce((a,b) => {return a*b}, 1)
 }
 
 
@@ -75,17 +71,18 @@ reverse(`hey`)
 
 // ---------------------
 // Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
-function findLongestWord([a,b,c,d]) {
-    let longest = " ";
-    for (let i =0, i < [a,b,c,d].length; i++) {
-        if ([a,b,c,d].length > longest) {
-            let longest = [a,b,c,d][0].length;
-            longest >= [a,b,c,d][i];
+function findLongestWord(words) {
+    let longest = "";
+    words.forEach(word => {
+        if (word.length > longest.length) {
+            longest = word;
         }
-        return longest;
-    }
+        
+    });
+    return longest;
+    
+
 }
-findLongestWord([`hi,hey,hello`]);
 
 // ---------------------
 // Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words
@@ -100,7 +97,19 @@ filterLongestWords([`hi`,`howdy`,`hello`,`yo`,`topofthemorning`],4)
 // Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it.
 //Represent the frequency listing as a Javascript object.
 //Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
-function charFreq( )
+function charFreq(word) {
+    const charCount = {}
+    const characters = word.split("") //creates an array of characters
+    characters.forEach(function(character){
+        if (charCount[character]) {
+            charCount[character]++
+        } else {
+            charCount[character] = 1
+
+        }
+    }) 
+    return charCount;
+}
 
 
 
